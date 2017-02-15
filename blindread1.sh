7223ad270b47ce872e5ca66c128c054c9c8406c7
Arvind <arvindprksh@gmail.com>
2017-02-16 01:05:38 +0530

    first change

--- blindread1.sh
+++ blindread1.sh
@@ -1,4 +1,13 @@
-convert img2.jpg -resize 1000 a.tif
-tesseract a.tif b
-python convert.py
-python TTS.py
+#!/bin/bash
+./textcleaner.sh  img2.jpg img21.jpg -g -e stretch -f 25 -o 20 -t 50 -u -s 1 -T -p 20
+#convert img2.jpg -crop 50%x100% +0 + 0 img.jpg
+convert img21.jpg  -resize 2000  -morphology close square:1 -threshold 90% a.tif 
+tesseract a.tif  b 
+#gedit b.txt
+printf '%b\n' "$(cat b.txt)"
+#festival --tts b.txt
+#python convert.py
+python ibmwatson.py
+cvlc output_en-US_AllisonVoice.wav 
+#-colorspace gray  +dither  -colors 2  -normalize 
+#-threshold 80%
