65fc15e3a5f9f6899b4b91f25957c76497643ba2
Arvind <arvindprksh@gmail.com>
2017-03-05 13:42:29 +0530

    inital build of ACE

--- pychat.py
+++ pychat.py
@@ -13,16 +13,37 @@ def chat(inp):
 		dest.write("Ok... lets start reading the book")
 		dest.close()
 		os.system('python ibmwatsonforace.py')
-		os.system('cvlc --play-and-exit output_en-US_AllisonVoice.wav &')
+		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')
 		os.system('./blindread1.sh ')
 		return 1
+	elif((inp.find("pause",0,len(inp))!=-1) or (inp.find("hold",0,len(inp))!=-1)):
+		"""dest = open("t2s.txt",'r+')
+		os.system(">t2s.txt")
+		dest.write("Ok... lets pause right here.. we will be back whenever you want")
+		dest.close()
+		os.system('pkill -9 vlc')
+		os.system('python ibmwatsonforace.py')
+		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')"""
+		os.system('pkill -STOP vlc')
+		return 1
+	elif((inp.find("continue",0,len(inp))!=-1) or (inp.find("resume",0,len(inp))!=-1)):
+		"""dest = open("t2s.txt",'r+')
+		os.system(">t2s.txt")
+		dest.write("Right.. Shall we begin now..")
+		dest.close()
+		os.system('python ibmwatsonforace.py')
+		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')"""
+		os.system('pkill -CONT vlc')
+		return 1
 	elif((inp.find("stop",0,len(inp))!=-1) and (inp.find("book",0,len(inp))!=-1)):
+		os.system('pkill vlc')
 		dest = open("t2s.txt",'r+')
 		os.system(">t2s.txt")
 		dest.write("ok.. i guess thats it for today")
 		dest.close()
 		os.system('python ibmwatsonforace.py')
-		os.system('cvlc --play-and-exit output_en-US_AllisonVoice.wav')
+		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav')
+		
 		return 1
 	elif((inp.find("define",0,len(inp))!=-1) or (inp.find("definition",0,len(inp))!=-1)):
 		print "ok"
@@ -30,10 +51,14 @@ def chat(inp):
 		os.system(">txt1.txt")
 		dest.write(inp)
 		dest.close()
+		os.system('pkill -STOP vlc')
 		os.system('python pydictionary1.py')
-		os.system('python ibmwatsonforace.py')
-		os.system('cvlc --play-and-exit output_en-US_AllisonVoice.wav')
+		os.system('python ibmwatsonfordict.py')
+		os.system('vlc --play-and-exit output_en-US_AllisonVoicefordict.wav')
+		os.system('pkill -CONT vlc')
 		return 1
+	elif((inp.find("quit",0,len(inp))!=-1) or (inp.find("exit",0,len(inp))!=-1)):
+		return 0
 
 val=1
 while(val!=0):
