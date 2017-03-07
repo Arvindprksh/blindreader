fa56ad64adb8a0cb9e4c46aa9559cda3e9406732
Arvind <arvindprksh@gmail.com>
2017-03-07 20:51:07 +0530

    added user recognition

--- pychat.py
+++ pychat.py
@@ -1,49 +1,30 @@
 import os
 import time
+def readitaloud(string):
+	dest = open("t2s.txt",'r+')
+	os.system(">t2s.txt")
+	dest.write(string)
+	dest.close()
+	os.system('python ibmwatsonforace.py')
+	os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav')
 
 def chat(inp):
-	#print inp
-	print inp.find("start",0,len(inp))
-	print inp.find("reading",0,len(inp))
-	print inp.find("stop",0,len(inp))
-	print inp.find("reading",0,len(inp))
 	if((inp.find("start",0,len(inp))!=-1) and (inp.find("reading",0,len(inp))!=-1)):
-		dest = open("t2s.txt",'r+')
-		os.system(">t2s.txt")
-		dest.write("Ok... lets start reading the book")
-		dest.close()
-		os.system('python ibmwatsonforace.py')
-		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')
-		os.system('./blindread1.sh ')
+		readitaloud("Ok... lets start reading the book")
+		os.system("./blindread1.sh")
 		return 1
 	elif((inp.find("pause",0,len(inp))!=-1) or (inp.find("hold",0,len(inp))!=-1)):
-		"""dest = open("t2s.txt",'r+')
-		os.system(">t2s.txt")
-		dest.write("Ok... lets pause right here.. we will be back whenever you want")
-		dest.close()
-		os.system('pkill -9 vlc')
-		os.system('python ibmwatsonforace.py')
-		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')"""
+		#readitaloud("Ok... lets pause right here.. we will be back whenever you want")
 		os.system('pkill -STOP vlc')
 		return 1
 	elif((inp.find("continue",0,len(inp))!=-1) or (inp.find("resume",0,len(inp))!=-1)):
-		"""dest = open("t2s.txt",'r+')
-		os.system(">t2s.txt")
-		dest.write("Right.. Shall we begin now..")
-		dest.close()
-		os.system('python ibmwatsonforace.py')
-		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')"""
+		#readitaloud("Right.. Shall we begin now..")
 		os.system('pkill -CONT vlc')
 		return 1
+
 	elif((inp.find("stop",0,len(inp))!=-1) and (inp.find("book",0,len(inp))!=-1)):
 		os.system('pkill vlc')
-		dest = open("t2s.txt",'r+')
-		os.system(">t2s.txt")
-		dest.write("ok.. i guess thats it for today")
-		dest.close()
-		os.system('python ibmwatsonforace.py')
-		os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav')
-		
+		readitaloud("ok.. i guess thats it for today")
 		return 1
 	elif((inp.find("define",0,len(inp))!=-1) or (inp.find("definition",0,len(inp))!=-1)):
 		print "ok"
@@ -57,10 +38,45 @@ def chat(inp):
 		os.system('vlc --play-and-exit output_en-US_AllisonVoicefordict.wav')
 		os.system('pkill -CONT vlc')
 		return 1
+
 	elif((inp.find("quit",0,len(inp))!=-1) or (inp.find("exit",0,len(inp))!=-1)):
+		os.system('pkill vlc')
 		return 0
 
+	else:
+		os.system('pkill -STOP vlc')
+		readitaloud("i am sorry can you repeat what you just said")
+		os.system('pkill -CONT vlc')
+		return 1
+
 val=1
+nfile=open("name.txt")
+nfile.seek(0)
+name=nfile.read(1)
+name1=nfile.read()
+nfile.close()
+if not name:
+	readitaloud("Hello , i am your very friendly reading assistant... May i know your name")
+	os.system('python googlestt.py')
+	src=open("s2t.txt")
+	inpu=src.read()
+	src.close()
+	#print inpu
+	nfile=open("name.txt","r+")
+	os.system(">name.txt")
+	nfile.write(inpu)
+	nfile.close()
+
+nfile1=open("name.txt")
+name1=nfile1.read()
+nfile.close()
+dest =  open("t2s.txt","r+")
+os.system(">t2s.txt")
+dest.write("Hello %s , i am your friendly reading assistant.. How can i be of service to you.." %name1)
+dest.close()
+os.system('python ibmwatsonforace.py')
+os.system('vlc --play-and-exit output_en-US_AllisonVoiceforace.wav &')
+
 while(val!=0):
 	os.system('python googlestt.py')
 	src=open("s2t.txt")
